7b1bc694f408d6434f360ad9e78e9de32496443a
VictarFH <vihe0001.stud.kea.dk>
2023-09-26 08:53:25 +0200

    adventure

---
+++ Adventure.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Adventure {
+    private Room currentRoom;
+
+    public Adventure() {
+        currentRoom = new Room("You are currently in your room. You just woke up from bed. ")
+    }
+}
---
+++ Room.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Room {
+    private String name;
+    private String description;
+
+    public Room(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        getDescription;
+    }
+}
