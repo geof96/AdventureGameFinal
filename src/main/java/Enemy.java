75c734c8729c466d85453333716516fe70cc2591
VictarFH <vihe0001.stud.kea.dk>
2023-10-09 22:21:20 +0200

    enemy klasse lavet

---
+++ src/main/java/Enemy.java
@@ -0,0 +1,41 @@
+public class Enemy {
+
+    private String enemyName;
+    private int enemyHealth;
+    private int enemyDamage;
+    private String enemyWeapon;
+
+    public Enemy (String enemyName, int enemyHealth, int enemyDamage, String enemyWeapon) {
+        this.enemyName = enemyName;
+        this.enemyHealth = enemyHealth;
+        this.enemyDamage = enemyDamage;
+        this.enemyWeapon = enemyWeapon;
+    }
+
+    public String getEnemyName() {
+        return enemyName;
+    }
+
+    public int getEnemyHealth() {
+        return enemyHealth;
+    }
+
+    public int getEnemyDamage() {
+        return enemyDamage;
+    }
+
+    public String getEnemyWeapon() {
+        return enemyWeapon;
+    }
+
+    @Override
+    public String toString() {
+        return "Enemy{" +
+                "enemyName='" + enemyName + '\'' +
+                ", enemyHealth=" + enemyHealth +
+                ", enemyDamage=" + enemyDamage +
+                ", enemyWeapon='" + enemyWeapon + '\'' +
+                '}';
+    }
+}
+
